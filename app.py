2ceb07bed1324493f7afb0a4304be62b5acd7216
ubairnoortech <ubair.noor@live.iium.edu.my>
2024-03-08 00:16:32 +0800

    images deleted

--- app.py
+++ app.py
@@ -14,14 +14,18 @@ def resize_image(img, new_width):
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        file = request.files['file']
+        files = request.files.getlist('file')  # Get list of uploaded files
         new_width = int(request.form['width'])
-        if file:
-            img = Image.open(file)
-            resized_img = resize_image(img, new_width)
-            filepath = f"static/resized_{file.filename}"
-            resized_img.save(filepath)
-            return render_template('index.html', resized_img=filepath)
+        resized_images = []
+        for file in files:
+            if file:
+                img = Image.open(file)
+                resized_img = resize_image(img, new_width)
+                filename = f"resized_{file.filename}"
+                filepath = os.path.join("static", filename)
+                resized_img.save(filepath)
+                resized_images.append(filepath)
+        return render_template('index.html', resized_images=resized_images)
     return render_template('index.html')
 
 if __name__ == '__main__':
