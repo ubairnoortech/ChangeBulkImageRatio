033de2e9840a698e6989f40e2d092efba7474f7a
ubairnoortech <ubair.noor@live.iium.edu.my>
2024-03-07 16:37:14 +0800

    Chnage image size for the bulk image

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from PIL import Image
-
+import os 
 
 def resize(img, new_width):
     width,height =  img.size
@@ -24,6 +24,18 @@ img = Image.open("images/BEEF.jpg")
 
 #imgr = img.resize((100,100)) # resize((w,h))
 
-im_resized = resize(img,200)
+# im_resized = resize(img,200)
 
-im_resized.show()
+# im_resized.show()
+
+files = os.listdir("images")
+
+print(files)
+extention = ['jpg','jpeg','png','gif']
+for file in files:
+    ext = file.split(".")[-1]
+    if ext in extention:
+        img = Image.open("images/"+file)
+        im_resized  = resize(img, 300)
+        filepath =  f"images/{file}.jpg"
+        im_resized.save(filepath)
