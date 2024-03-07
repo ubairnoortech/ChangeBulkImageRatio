3489ad95d92718d34541dac9ce4f8f8bb2db8a12
ubairnoortech <ubair.noor@live.iium.edu.my>
2024-03-07 16:43:54 +0800

    Adding the flask framework

--- app.py
+++ app.py
@@ -1,41 +1,28 @@
+from flask import Flask, render_template, request 
 from PIL import Image
-import os 
+import os
 
-def resize(img, new_width):
-    width,height =  img.size
-    ratio = height/width
-    print(ratio)
+app = Flask(__name__)
+
+def resize_image(img, new_width):
+    width, height = img.size
+    ratio = height / width
     new_height = int(ratio * new_width)
-    print(new_height)
-    print(new_width)
-    resized_image =  img.resize((new_width,new_height))
-    print(new_width)
+    resized_image = img.resize((new_width, new_height))
     return resized_image
- 
-img = Image.open("images/BEEF.jpg")
 
-#print(img)
-# we found img variable has some properites  such as size
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        file = request.files['file']
+        new_width = int(request.form['width'])
+        if file:
+            img = Image.open(file)
+            resized_img = resize_image(img, new_width)
+            filepath = f"static/resized_{file.filename}"
+            resized_img.save(filepath)
+            return render_template('index.html', resized_img=filepath)
+    return render_template('index.html')
 
-#print(img.size) # return ( w:2940,h: 3920)
-
-# so it has also  a methods such as 
-
-
-#imgr = img.resize((100,100)) # resize((w,h))
-
-# im_resized = resize(img,200)
-
-# im_resized.show()
-
-files = os.listdir("images")
-
-print(files)
-extention = ['jpg','jpeg','png','gif']
-for file in files:
-    ext = file.split(".")[-1]
-    if ext in extention:
-        img = Image.open("images/"+file)
-        im_resized  = resize(img, 300)
-        filepath =  f"images/{file}.jpg"
-        im_resized.save(filepath)
+if __name__ == '__main__':
+    app.run(debug=True)
