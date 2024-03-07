09a059858e42ccee7c254ce1c26206decacaa7c0
ubairnoortech <ubair.noor@live.iium.edu.my>
2024-03-07 15:43:39 +0800

    using the img properties such as size, resize

--- app.py
+++ app.py
@@ -2,4 +2,12 @@ from PIL import Image
 
 img = Image.open("images/BEEF.jpg")
 
-img.show()
+print(img)
+# we found img variable has some properites  such as size
+
+print(img.size) # return ( w:2940,h: 3920)
+
+# so it has also  a methods such as 
+
+imgr = img.resize((100,100)) # resize((w,h))
+imgr.show()
