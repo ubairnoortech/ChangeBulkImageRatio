7aef097dc4b50177c40fe7727d8dd74ac245d0f5
ubairnoortech <ubair.noor@live.iium.edu.my>
2024-03-07 15:55:15 +0800

    Resize the image with aspect ratio

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
 from PIL import Image
 
+
+def resize(img, new_width):
+    width,height =  img.size
+    ratio = height/width
+    print(ratio)
+    new_height = int(ratio * new_width)
+    print(new_height)
+    print(new_width)
+    resized_image =  img.resize((new_width,new_height))
+    print(new_width)
+    return resized_image
+ 
 img = Image.open("images/BEEF.jpg")
 
-print(img)
+#print(img)
 # we found img variable has some properites  such as size
 
-print(img.size) # return ( w:2940,h: 3920)
+#print(img.size) # return ( w:2940,h: 3920)
 
 # so it has also  a methods such as 
 
-imgr = img.resize((100,100)) # resize((w,h))
-imgr.show()
+
+#imgr = img.resize((100,100)) # resize((w,h))
+
+im_resized = resize(img,200)
+
+im_resized.show()
